b8fa8b90d15e8c14d1d9044ed3d6eef0f71b52a1
Rion <rion.it456@gmail.com>
2023-02-03 13:34:08 +0900

    chenged layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,16 +8,23 @@ import {
   TwoColumnSidebar,
 } from "components/TwoColumn";
 import Image from "next/image";
-import eyecatch from "images/about.jpg"
+import eyecatch from "images/about.jpg";
 import Meta from "components/Meta";
+import styles from "src/styles/about.module.css";
 
 const About = () => {
   return (
     <Container>
-      <Meta pageTitle="About" pageDesc="私について" pageImg={eyecatch.src} pageImgW={eyecatch.width} pageImgH={eyecatch.height}/>
+      <Meta
+        pageTitle="About"
+        pageDesc="私について"
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
       <Hero title="About" subtitle="私について" />
       <figure>
-        <Image 
+        <Image
           src={eyecatch}
           alt=""
           layout="responsive"
@@ -30,20 +37,32 @@ const About = () => {
       <TwoColumn>
         <TwoColumnMain>
           <PostBody>
-            <p>
-              Cubeが得意とする分野はモノづくりです。3次元から2次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
-            </p>
-            <h2>モノづくりで目指していること</h2>
+            <h2>自己紹介</h2>
             <p>
               モノづくりではデータの解析からクリエイティブまで幅広いことを担当しています。新しいことを取り入れながら、ユーザーにマッチした提案を実現するのが目標です。たくさんの開発・提供が数多くありますが、特にそこを磨く作業に力を入れています。
             </p>
             <p>
               単純に形にするだけでなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくりをしています。毎回課題解決テーマをもって「モノ」と向き合い制作をし、フィードバックしてもらうことで自分の中にあるモヤモヤを言葉にして「問い」への答えを出しています。
             </p>
-            <h3>新しいことへのチャレンジ</h3>
-            <p>
-              今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
-            </p>
+
+            <h2>これまでにやってきたこと/取得した資格</h2>
+
+            <div className={styles.grid}>
+              <h4 className={styles.title}>資格</h4>
+              <ul className={styles.listStyle}>
+                <li>MOS(Microsoft Office Specialist)</li>
+                <li>基本情報技術者</li>
+              </ul>
+            </div>
+            <hr />
+            <div className={styles.grid}>
+              <h4 className={styles.title}>スキル</h4>
+              <ul className={styles.listStyle}>
+                <li>HTML, CSS, JavaScript, React, Next.js</li>
+                <li>Node.js, Python, Java</li>
+                <li>Figma</li>
+              </ul>
+            </div>
           </PostBody>
         </TwoColumnMain>
         <TwoColumnSidebar>
